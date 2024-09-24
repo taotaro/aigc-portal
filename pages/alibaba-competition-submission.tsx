438b3ae524a6f818d20fcaff415fc5ef27f068ee
kavyagb22 <kgbalaji@connect.ust.hk>
2024-09-24 18:30:28 +0800

    test commit for deployment

--- pages/alibaba-competition-submission.tsx
+++ pages/alibaba-competition-submission.tsx
@@ -14,6 +14,7 @@ export default function AlibabaCompetitionSubmission() {
     }
 
     const throttledResize = throttle(onWindowResize, 200);
+    console.log("test");
 
     useEffect(() => {
         window.scrollTo(0, 0);
