f35837e985bd6c186221354c86a9dfe9d8f8588a
kavyagb22 <kgbalaji@connect.ust.hk>
2024-09-25 17:10:21 +0800

    url changed to prod for main

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -357,7 +357,8 @@ export default function Registration() {
         try {
             console.log("payload: ", payload);
             const response = await axios.post(
-                "https://aigc-backend-dev.materia-logic.com/common/register",
+                // "https://aigc-backend-dev.materia-logic.com/common/register",
+                "https://alibabacloudtongyi.com.hk/common/register",
                 // "http://127.0.0.1:8000/common/register",
                 payload
             );
@@ -402,8 +403,8 @@ export default function Registration() {
             // console.error(error);
             console.log("error", error, typeof error);
             const response = await axios.post(
-                // "https://alibabacloudtongyi.com.hk/common/log-error",
-                "https://aigc-backend-dev.materia-logic.com/common/log-error",
+                "https://alibabacloudtongyi.com.hk/common/log-error",
+                // "https://aigc-backend-dev.materia-logic.com/common/log-error",
                 // "http://127.0.0.1:8000/common/log-error",
                 { error: error }
             );
