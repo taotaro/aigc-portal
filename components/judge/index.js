5799d63f8bbb1a3037b49257dba12516871fda19
kavyagb22 <kgbalaji@connect.ust.hk>
2024-10-17 17:18:17 +0800

    judges panel added

--- components/judge/index.js
+++ components/judge/index.js
@@ -1,39 +1,43 @@
 import React from "react";
 import useTranslation from "next-translate/useTranslation";
 
-export default function Members({ judgeName, judgeDescription }) {
-  const { t } = useTranslation("common");
-  //   const { name, value } = e.target;
-  //   onJudgeDataChange({ ...judgeData, [name]: value });
+export default function Members({
+    judgeName,
+    judgeDescription,
+    judgeInfo,
+    judgePhoto,
+}) {
+    const { t } = useTranslation("common");
+    //   const { name, value } = e.target;
+    //   onJudgeDataChange({ ...judgeData, [name]: value });
 
-  const handleInputChange = (e) => {};
-  return (
-    <>
-      <div
-        style={{
-          padding: "40px",
-          textAlign: "center",
-          borderColor: "black",
-          border: "1px solid black",
-          borderRadius: "8px",
-          borderWidth: "1px",
-          maxWidth: "20%",
-          minWidth: "250px",
-          // background: "black",
-        }}
-      >
-        <div style={{ paddingBottom: "24px" }}>
-          <div
-            style={{
-              fontSize: "24px",
-              fontWeight: "bold",
-            }}
-          >
-            {judgeName}
-          </div>
-        </div>
-        <div>{judgeDescription}</div>
-      </div>
-    </>
-  );
+    const handleInputChange = (e) => {};
+    return (
+        <>
+            <div
+                style={{
+                    padding: "40px",
+                    textAlign: "center",
+                    borderColor: "black",
+                    // border: "1px solid black",
+                    borderRadius: "8px",
+                    borderWidth: "1px",
+                    maxWidth: "20%",
+                    minWidth: "250px",
+                    background: "#D3D3D3",
+                }}
+            >
+                <div key={judgeName} style={{ paddingBottom: "24px" }}>
+                    <img
+                        className="module-logo__img"
+                        src={judgePhoto}
+                        alt={judgeName}
+                    />
+                </div>
+                <div>{judgeName}</div>
+                <div>{judgeDescription}</div>
+                <div>{judgeInfo}</div>
+            </div>
+        </>
+    );
 }
