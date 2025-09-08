9f3e0ae6dc681ee8ba52777b211b1ee4d6c6d56a
taobaozhong <1336945839@qq.com>
2025-09-08 20:41:49 +0800

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,21 @@ const nextTranslate = require("next-translate");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 配置全局重定向到阿里云通义千问
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://alibabacloudtongyi.com/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 // module.exports = nextConfig
 module.exports = nextTranslate({
+  ...nextConfig,
   webpack: (config, { isServer, webpack }) => {
     return config;
   },
